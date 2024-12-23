fb3752db5991ada8fe54e23b994bb538f9135647
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2024-12-23 23:10:06 +0800

    javascript implemented

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.getElementById('box1');
+  
+    box.addEventListener('mouseover', () => {
+      box.style.border = '1px solid #106CBD'; // Add border on hover
+    });
+  
+    box.addEventListener('mouseout', () => {
+      box.style.border = 'none'; // Remove border when not hovering
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.getElementById('box2');
+  
+    box.addEventListener('mouseover', () => {
+      box.style.border = '1px solid #106CBD'; // Add border on hover
+    });
+  
+    box.addEventListener('mouseout', () => {
+      box.style.border = 'none'; // Remove border when not hovering
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.getElementById('box3');
+  
+    box.addEventListener('mouseover', () => {
+      box.style.border = '1px solid #106CBD'; // Add border on hover
+    });
+  
+    box.addEventListener('mouseout', () => {
+      box.style.border = 'none'; // Remove border when not hovering
+    });
+  });
